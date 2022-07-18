d5bead710a13e1e1cadf564e307bb5cc7b1d1717
Francisco José Lavalle <franlavalle@hotmail.com>
2022-07-18 19:13:54 -0300

    Agrgo grafico

--- src/components/Grafico/Grafico.jsx
+++ src/components/Grafico/Grafico.jsx
@@ -1,22 +1,44 @@
 import { useContext } from 'react';
 import { ModalContext } from '../../context/ModalContext';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto';
 
 import './Grafico.css';
 
 function Grafico() {
     const { amounts } = useContext(ModalContext);
     console.log(amounts);
-    let estilos = {
-        backgroundImage: `conic-gradient(green 10%, red 10%, grey 10%)`,
-        width: '10rem',
-        height: '10rem',
-        borderRadius: '50%'
-    }
+
+    let amountsEgresos = amounts.filter(amount => amount.type == 'Egreso').map(amount => parseFloat(amount.amount));
+
+    let amountsCategorias = amounts.filter(amount => amount.type == 'Egreso').map(amount => amount.categoria);
+
+    console.log(amountsEgresos);
+    console.log(amountsCategorias);
+
     return (
         <>
-            <h2>Gráfico</h2>
-            <div className='grafico-cont' style={estilos}>
-            </div>
+            <Pie
+                data={{
+                    labels: amountsCategorias,
+                    datasets: [{
+                        label: '# of votes',
+                        data: amountsEgresos,
+                        backgroundColor: [
+                            '#333',
+                            '#f00',
+                            '#00f'
+                        ],
+                        borderColor: [
+                            '#444',
+                            '#f11',
+                            '#11f'
+                        ]
+                    }]
+                }}
+                height={10000}
+                width={10000}
+            />
         </>
     )
 }
