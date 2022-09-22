2a6935c9769f2e17c4e8d5670e6b2dc26480097b
Francisco José Lavalle <franlavalle@hotmail.com>
2022-09-21 22:24:51 -0300

    Agrego localStorage

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -5,14 +5,14 @@ export const ModalContext = createContext([])
 function ModalContextProvider({ children }) {
   const [modal, setModal] = useState(false);
   const [editItem, setEditItem] = useState(false);
-  const [amounts, setAmounts] = useState([]);
+  const [amounts, setAmounts] = useState(JSON.parse(localStorage.getItem('amounts')) || []);
   const [amountWithoutFilter, setAmountWithoutFilter] = useState([]);
   const [ingresos, setIngresos] = useState([]);
   const [egresos, setEgresos] = useState([]);
   const [id, setId] = useState(0);
   const [finalAmount, setFinalAmount] = useState(0);
   const [showAmounts, setShowAmounts] = useState([]);
-  const [ciclos, setCiclos] = useState([]);
+  const [ciclos, setCiclos] = useState(JSON.parse(localStorage.getItem('ciclos')) || []);
   const [cicloNombre, setCicloNombre] = useState('');
   const [nombreCiclo, setNombreCiclo] = useState(false);
 
@@ -21,6 +21,8 @@ function ModalContextProvider({ children }) {
     setEgresos(amounts.filter(el => el.type === 'Egreso').map(element => parseFloat(element.amount)));
     setFinalAmount(ingresos.reduce((anterior, siguiente) => anterior + siguiente, 0) - egresos.reduce((anterior, siguiente) => anterior + siguiente, 0))
     setShowAmounts(amounts);
+    localStorage.setItem('ciclos',JSON.stringify(ciclos));
+    localStorage.setItem('amounts', JSON.stringify(amounts));
   },[amounts])
   useEffect(() => {
     setIngresos(amounts.filter(el => el.type === 'Ingreso').map(element => parseFloat(element.amount)));
@@ -40,6 +42,8 @@ function ModalContextProvider({ children }) {
     setEditItem(true);
   }
 
+
+
   function guardarCiclo() {
     let fecha = new Date();
     let day = fecha.getDate();
@@ -51,8 +55,10 @@ function ModalContextProvider({ children }) {
     
     let textFecha = `${day}/${month + 1}/${year} a las ${hour}:${minute}:${second}`;
     setCiclos([...ciclos, {id: Date.now(), name: cicloNombre, fecha: textFecha, elementos: amounts}]);
+    console.log(ciclos);
     setAmounts([]);
     setNombreCiclo(false);
+    localStorage.setItem('ciclos',JSON.stringify(ciclos));
   }
 
   return (
